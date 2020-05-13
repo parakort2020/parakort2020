301e89027e784d4747031e3acf8e14b7c664bb53
sergey <sergei.merlow@ya.ru>
2020-05-13 22:15:16 +0300

    14 lesson

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'><div>
+    return <div className={s.content}><div>
     <img src="https://img3.akspic.ru/image/33593-fruktovoe_derevo-mestnoe_blyudo-apelsin-kozhura-pishha-1920x1080.jpg"/></div>
     <div>
       ava + discription
@@ -12,8 +13,8 @@ const Profile = () => {
     <div>
       New post
     </div>
-    <div>post 1</div>
-    <div>post 2</div>
+    <div className={s.item}>post 1</div>
+    <div className={s.item}>post 2</div>
     </div>
     
 }
